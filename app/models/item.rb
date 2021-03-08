645119046f5e291e1b6d8994ba5d16f7d7a1abc6
mitsu23273552 <w54t5984@yahoo.co.jp>
2021-03-08 12:18:33 +0900

    ActiveStorage導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+  def was_attached?
+    self.image.attached?
+  end
 
   with_options presence: true do
     validates :title
