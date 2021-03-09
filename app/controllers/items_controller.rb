1ae01b8385cafcc8761ffdb6402326c108f2434c
mitsu23273552 <w54t5984@yahoo.co.jp>
2021-03-09 17:21:04 +0900

    出品情報のデータベース保存機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,19 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @item =Item.new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:user_id, :title, :item_comment, :category_id, :price, :item_quality_id, :delivery_fee_id, :prefecture_id, :delivery_day_id).merge(user_id: current_user.id)
   end
 end
