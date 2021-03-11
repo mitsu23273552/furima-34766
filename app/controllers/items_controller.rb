ae18d37160b5f8bcf5f4431b0e75a5895eafdd70
mitsu23273552 <w54t5984@yahoo.co.jp>
2021-03-11 18:21:27 +0900

    商品編集ページの作成
    ルーティング・ビューファイル設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :item_param_set, only: [:edit, :show]
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -18,11 +20,21 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 
   def item_params
     params.require(:item).permit(:user_id, :title, :item_comment, :category_id, :price, :item_quality_id, :delivery_fee_id,
                                  :prefecture_id, :delivery_day_id, :image).merge(user_id: current_user.id)
   end
+
+  def item_param_set
+    @item = Item.find(params[:id])
+  end
+
 end
