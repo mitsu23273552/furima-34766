810f2204f7f62a120e90cbc2abcbddf8e0e60e20
mitsu23273552 <w54t5984@yahoo.co.jp>
2021-03-18 12:35:05 +0900

     不要なバリデーションの削除

--- app/models/to_address.rb
+++ app/models/to_address.rb
@@ -3,15 +3,6 @@ class ToAddress < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
-
-  with_options presence: true do
-    validates :to_postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :to_city
-    validates :to_address1
-    validates :to_telephone_number, format: { with: /\A[0-9]+$\z/ }, length: { maximum: 11 }
-
-    with_options numericality: { other_than: 1 } do
-      validates :prefecture_id
     end
   end
 end
